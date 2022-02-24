509544e91e2fb33f29a97a57aca839bcef910b82
Avish Kumar <80064807+avishmonga@users.noreply.github.com>
2022-02-24 10:28:48 +0530

    init

---
+++ src/components/Genre.jsx
@@ -0,0 +1,192 @@
+// import React, { useEffect, useState } from "react";
+// import styled from "styled-components";
+// import Slider from "react-slick";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import { useSelector } from "react-redux";
+// function Genre(props) {
+//   let settings = {
+//     dots: false,
+//     infinite: false,
+//     speed: 500,
+//     slidesToShow: 8,
+//     slidesToScroll: 1,
+//     autoplay: false,
+//   };
+// const [data2,setdata2] = useState([])
+//  let {data,lang} = useSelector((store)=>store)
+// let filterddata = [];
+//   return (
+//     <Container>
+//       <h3>{props.title}</h3>
+//       <Carousel {...settings}>
+//       {/* { filterddata = data.map((element) => {
+//   return {...element, genre_ids: element.genre_ids.filter((subElement) => subElement === 18)}
+// })}
+//        {console.log(filterddata)} */}
+//         {data.map((e) => {
+//            return (
+//             <div>
+//                <img
+//             className="img-card"
+//               src={`https://image.tmdb.org/t/p/w500${e.poster_path}            `}
+            
+//             />
+
+//             </div>
+           
+//           );
+//         })}
+//       </Carousel>
+//     </Container>
+//   );
+// }
+
+// export default Genre;
+// const Container = styled.div`
+//   margin: 30px;
+
+//   h3 {
+//     color: white;
+//     margin: 10px;
+//   }
+// `;
+// const Wrap = styled.div`
+
+//   gap:10px;
+//   /* box-sizing: border-box; */
+
+  
+// `;
+// const Carousel = styled(Slider)`
+
+// height: 100%;
+// margin-left: 2%;
+// /* display: flex; */
+//   .slick-list {
+//     overflow: visible;
+//   }
+//   img {
+//     width:95%;
+//   height: 240px;
+//   transition: 0.3s;
+
+//   &:hover{
+//     transform: translate(0, -15px);
+//   }
+//   }
+//   .slick-prev{
+//     left:-30px;
+//     z-index:1;
+  
+//   }
+//     .slick-disabled{
+     
+//       &::before{
+//         display:none;
+//         cursor: none;
+//       }
+//     }
+   
+// `;
+
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+function Genre(props) {
+  let settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: false,
+  };
+
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=024bccde6bb621415ae4cb4cddc07d56&with_original_language=te&with_genres=${props.genre}`
+      )
+      .then(function (response) {
+        // console.log(response.data.results)
+        setdata(response.data.results);
+        
+      });
+  }, []);
+
+  return (
+    <Container>
+      <h3>{props.title}</h3>
+      <Carousel {...settings}>
+        {data.map((e) => {
+          return (
+            <div>
+               <img
+            className="img-card"
+              src={`https://image.tmdb.org/t/p/w500${e.poster_path}            `}
+            
+            />
+
+            </div>
+           
+          );
+        })}
+      </Carousel>
+    </Container>
+  );
+}
+
+export default Genre;
+const Container = styled.div`
+  margin: 30px;
+
+  h3 {
+    color: white;
+    margin: 10px;
+  }
+`;
+const Wrap = styled.div`
+
+  gap:10px;
+  /* box-sizing: border-box; */
+
+  
+`;
+const Carousel = styled(Slider)`
+
+height: 100%;
+margin-left: 2%;
+/* display: flex; */
+  .slick-list {
+    overflow: visible;
+  }
+  img {
+    width:95%;
+  height: 240px;
+  transition: 0.3s;
+
+  &:hover{
+    transform: translate(0, -15px);
+  }
+  }
+  .slick-prev{
+    left:-30px;
+    z-index:1;
+  
+  }
+    .slick-disabled{
+     
+      &::before{
+        display:none;
+        cursor: none;
+      }
+    }
+   
+`;
+
---
+++ src/redux/ActionTypes.js
@@ -0,0 +1,4 @@
+export const ADD_LOGIN = 'ADD_LOGIN'
+export const ADD_PAYMENT = 'ADD_PAYMENT'
+export const ADD_LANG = 'ADD_LANG'
+export const ADD_DATA = 'ADD_DATA'
