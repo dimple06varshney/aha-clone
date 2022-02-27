6a0faf5b1cfa21ca5f8792aa0291cd18b47e089b
Avish Kumar <80064807+avishmonga@users.noreply.github.com>
2022-02-27 10:49:59 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -1,48 +1,3 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-<<<<<<< Updated upstream
-import Imgslider from './components/imgSlider';
-import Movies from './components/NewReleases';
-import Genre from './components/Genre';
-
-=======
-import {Routes, Route} from "react-router-dom";
-import Home from "./components/Home";
-import Login from './components/login/Login'; 
-import Subscribe from './components/Subscribe';
-import { DetailsPage } from './components/DetailsPage';
-import { requests } from './requests';
->>>>>>> Stashed changes
-function App() {
- 
-  return (
-    <div className="App">
-<<<<<<< Updated upstream
-      <Imgslider />
-      <Movies title='New Releases' />
-      <Movies  title='Top-10 Movies this week' />
-      <Genre title="Action" genre = {28} />
-      <Genre title="Comedy" genre = {35} />
-      <Genre title="Crime" genre = {80} />
-      <Genre title="Drama" genre = {18} />
-      <Genre title="Romance" genre = {10749} />
-      <Genre title="Thriller" genre = {53} />
-
-=======
-      <Routes >
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/signin' element={<Login/>}></Route>
-        <Route path='/subscribe' element={<Subscribe/>}></Route>
-        <Route path='/details/:id' element={<DetailsPage />}></Route>
-      </Routes>
->>>>>>> Stashed changes
-    </div>
-  );
-}
-
-export default App;
-=======
 import './App.css';
 import {Routes, Route} from "react-router-dom";
 import Home from "./components/Home";
@@ -68,4 +23,3 @@ function App() {
 }
 
 export default App;
->>>>>>> Stashed changes
--- src/axios.js
+++ src/axios.js
@@ -1,12 +1,5 @@
-<<<<<<< Updated upstream
 import axios from "axios";
 
 export const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
-=======
-import axios from "axios";
-
-export const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
->>>>>>> Stashed changes
 });
